0afc67c20c7ec6a4d2253ece7155b065872c9e62
dev-zam <zamsyh.work@gmail.com>
2020-10-10 02:03:25 +0700

    Initial commit

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const cors = require('cors');
 const http = require('http');
 const conn = require('./env/index');
 
+const insertRoom = require('./view/insert');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/room", insertRoom);
 
 app.all("*", (req, res) => {
     res.send({message: "The router or endpoint you entered was not found"});
