02b5cecd893737bf2ae5dc71d26bd2461bec318c
dev-zam <zamsyh.work@gmail.com>
2020-10-10 08:23:43 +0700

    fix little bug

--- controller/helper.js
+++ controller/helper.js
@@ -3,7 +3,7 @@ const generateRoom = (param = {}) => {
     let res = new Object();
     const base_url = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=";
     res.emailSiswa = param.emailSiswa;
-    res.nameRoom = nameSlug(param.nameRoom);
+    res.nameRoom = param.nameRoom;
     res.iconName = base_url + nameSlug(param.nameRoom);
     return res;
 }
