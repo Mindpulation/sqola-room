3930a47d6a06d19d37c85bd80dbc47d857f6becb
dev-zam <zamsyh.work@gmail.com>
2020-10-10 10:10:32 +0700

    Update generateRoom using regex

--- controller/helper.js
+++ controller/helper.js
@@ -1,11 +1,24 @@
-const nameSlug = require('url-slug');
+// const nameSlug = require('url-slug');
+// const generateRoom = (param = {}) => {
+//     let res = new Object();
+//     const base_url = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=";
+//     res.emailSiswa = param.emailSiswa;
+//     res.nameRoom = param.nameRoom;
+//     res.iconName = base_url + nameSlug(param.nameRoom);
+//     return res;
+// }
+
 const generateRoom = (param = {}) => {
     let res = new Object();
-    const base_url = "https://ui-avatars.com/api/?background=0D8ABC&color=fff&name=";
     res.emailSiswa = param.emailSiswa;
     res.nameRoom = param.nameRoom;
-    res.iconName = base_url + nameSlug(param.nameRoom);
+
+    var regex = param.nameRoom.match(/\b\w/g) || [];
+    regex = ((regex.shift() || '') + (regex.pop() || '')).toUpperCase();
+    
+    res.iconRoom = regex;
     return res;
 }
+
 
 module.exports = { generateRoom }
